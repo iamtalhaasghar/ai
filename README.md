ac273adc625caf7096253be8240fa5d06c07998f
Talha Asghar <talhaasghar.contact@simplelogin.fr>
2022-02-22 16:37:14 +0500

    added credits

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# mathematical-models-for-machine-learning
+## My practice code for different AI, ML, DL algorithms & techniques. 
+
+# Credits:
+1. https://github.com/llSourcell/linear_regression_demo
--- linear_regression.py
+++ linear_regression.py
@@ -2,21 +2,13 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 import pandas
 
-train_data = pandas.read_csv('train.csv')
-train_data.fillna(0, inplace=True)
-print(train_data.head)
-train_features = train_data[['x']]
-print(train_features)
-train_labels = train_data[['y']].values.tolist()
-print(train_labels)
-
-test_data = pandas.read_csv('test.csv')
-test_data.fillna(0, inplace=True)
-test_features = train_data[['x']].values.tolist()
-test_labels = train_data[['y']].values.tolist()
+df = pandas.read_csv('data.csv')
+df.fillna(0, inplace=True)
+x = df[['x']]
+y = df[['y']]
 
 ai = linear_model.LinearRegression()
-ai.fit(train_features, train_labels)
-plt.scatter(test_features, test_labels)
-plt.plot(test_features, ai.predict(test_features))
+ai.fit(x, y)
+plt.scatter(x, y)
+plt.plot(x, ai.predict(x))
 plt.show()
